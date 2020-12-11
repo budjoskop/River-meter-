350b290e054bc6014bdfb5e9335ebdf92349184d
Ognjen Tomic <ognjen.tomic@webedia-group.com>
2020-12-11 21:38:56 +0100

    Initial Commit

---
+++ River Meter/River_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  River_MeterApp.swift
+//  River Meter
+//
+//  Created by Ognjen Tomic on 11.12.20..
+//
+
+import SwiftUI
+
+@main
+struct River_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
